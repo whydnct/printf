1321d514e041cc375c4d00e4e3d0b17294b4dcac
al <whydnct@gmail.com>
2022-12-11 09:08:14 +0100

    init prinft and update libprintf.h

---
+++ libprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libprintf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aperez-m <aperez-m@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/11 08:55:06 by aperez-m          #+#    #+#             */
+/*   Updated: 2022/12/11 09:07:07 by aperez-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBPRINTF_H
+# define LIBPRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "./libft/libft.h"
+
+int	ft_printf(char const *, ...);
+#endif
