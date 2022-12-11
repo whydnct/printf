f6a7c8f5fb59fe513215ce52aa6ce4d8a2baa6f3
al <whydnct@gmail.com>
2022-12-11 16:12:54 +0100

    .

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,27 @@
 /*   By: aperez-m <aperez-m@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 08:50:49 by aperez-m          #+#    #+#             */
-/*   Updated: 2022/12/11 09:07:11 by aperez-m         ###   ########.fr       */
+/*   Updated: 2022/12/11 16:11:03 by aperez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libprintf.h"
 
-int	ft_printf(char const *, ...)
+int	ft_printf(char const *fmt, ...)
 {
+	va_list	ap;
+	int		ret;
 
+	va_start(ap, fmt);
+	if (!fmt)
+		return (0);
+	while (*fmt)
+	{
+		if (*fmt != '%')
+			ft_putchar_fd(fmt, 1);
+		else
+			fmt += choose_printer(fmt, va_arg);
+		fmt++; 
+	}
+	return (ret)
 }
--- libprintf.h
+++ libprintf.h
@@ -6,7 +6,7 @@
 /*   By: aperez-m <aperez-m@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 08:55:06 by aperez-m          #+#    #+#             */
-/*   Updated: 2022/12/11 09:07:07 by aperez-m         ###   ########.fr       */
+/*   Updated: 2022/12/11 12:06:50 by aperez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdarg.h>
 # include "./libft/libft.h"
 
 int	ft_printf(char const *, ...);
