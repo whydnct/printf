f76e3c69df1c654378e27dcf82a02db5fb6b0189
al <whydnct@gmail.com>
2022-12-14 22:53:13 +0100

    update printers

--- print_hex_long.c
+++ print_hex_long.c
@@ -1,38 +1,54 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_long.c                                       :+:      :+:    :+:   */
+/*   print_hex_long.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aperez-m <aperez-m@student.42urduliz.co    +#+  +:+       +#+        */
+/*   By: aperez-m <aperez-m@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 14:45:40 by aperez-m          #+#    #+#             */
-/*   Updated: 2022/12/14 20:36:13 by aperez-m         ###   ########.fr       */
+/*   Updated: 2022/12/14 22:51:29 by aperez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 
-int	print_long(unsigned long nbr, int lowcase, int prefix)
+static	char	to_hex(unsigned long nbr, int lowcase)
+{
+	if (nbr < 10)
+		return (nbr + '0');
+	else
+		return (nbr - 10 + 'A' + lowcase * ('a' - 'A'));
+}
+
+int	print_unsigned(unsigned long nbr, int lowcase, int prefix, char c)
 {
 	char			*nbr_str;
 	int				i;
+	char			*base;
+	int				base_length;
 
+	base = "0123456789abcdef";
+	base_length = 16;
+	if (c == 'u')
+		base_length = 10;
+	if (c == 'X')
+		base = "0123456789ABCDEF";
 	i = -1;
 	nbr_str = malloc(17);
 	while (i++ < 17)
 		nbr_str[i] = 0;
 	i = 15;
-	while (nbr / 16)
+	while (nbr / base_length)
 	{
-		nbr_str[i] = to_hex((nbr % 16), lowcase);
-		nbr /= 16;
+		nbr_str[i] = base[nbr % base_length];
+		nbr /= base_length;
 		i--;
 	}
-	nbr_str[i] = to_hex((nbr % 16), lowcase);
+	nbr_str[i] = base[nbr % base_length];
 	i--;
-	if (prefix == 1)
+	if (c == 'p')
 		ft_putstr_fd("0x", 1);
 	ft_putstr_fd(nbr_str + i + 1, 1);
 	free(nbr_str);
-	return (2*(int)(prefix == 1) + 16 - i - 1);
+	return (2 * (int)(c == 'p') + base_length - i - 1);
 }
