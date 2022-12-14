3190a5adcf94c7596657d987187841b0fc29bbf1
al <whydnct@gmail.com>
2022-12-14 19:18:36 +0100

    printf without bonus

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: aperez-m <aperez-m@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 08:50:49 by aperez-m          #+#    #+#             */
-/*   Updated: 2022/12/14 18:40:13 by aperez-m         ###   ########.fr       */
+/*   Updated: 2022/12/14 19:17:41 by aperez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int	ft_printf(char const *str, ...)
 {
 	va_list	ap;
 	int		ret;
-	int		flags_nbr;
 
 	if (!str)
 		return (0);
@@ -32,22 +31,10 @@ int	ft_printf(char const *str, ...)
 		else
 		{
 			str++;
-			flags_nbr = 0;
-			while (is_flag(*str))
-			{
-				str++;
-				flags_nbr++;
-			}
 			if (is_formatter(*str))
-			{
-				if (flags_compatible(str, flags_nbr))
-					ret += choose_printer(str, flags_nbr, ap);
-				else
-					return (abort_execution());
-			}
+				ret += choose_printer(str, ap);
 			else
 				return (abort_execution());
-		str++;
 		}
 	}
 	return (ret);
