db0e0b56b50257e89b87ad2435e9979c4bff5bef
aperez-m <aperez-m@student.42urduliz.com>
2022-12-19 16:35:57 +0100

    Ãremove redunant rules for x çX and u

--- choose_printer.c
+++ choose_printer.c
@@ -6,7 +6,7 @@
 /*   By: aperez-m <aperez-m@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 19:20:54 by aperez-m          #+#    #+#             */
-/*   Updated: 2022/12/19 12:33:37 by aperez-m         ###   ########.fr       */
+/*   Updated: 2022/12/19 16:34:25 by aperez-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int	choose_printer(const char *str, va_list ap)
 		return (print_int(va_arg(ap, int)));
 	if (*str == 'x' || *str == 'X' || *str == 'u')
 		return (print_long((unsigned int)(va_arg(ap, unsigned long)), *str));
-	if (*str == 'x' || *str == 'X' || *str == 'p')
+	if (*str == 'p')
 		return (print_long(va_arg(ap, unsigned long), *str));
 	return (0);
 }
